e867360cfdfea1aab659733cae11c8abb42aa2e7
Joe Grandja <10884212+jgrandja@users.noreply.github.com>
2024-06-07 10:24:08 -0400

    Add feature configuration for jwt-login use case

---
+++ config-feature/src/main/java/org/springframework/security/config/feature/model/OAuth2ResourceServerFeature.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2002-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.config.feature.model;
+
+import java.util.Map;
+
+/**
+ * @author Joe Grandja
+ */
+public final class OAuth2ResourceServerFeature extends Feature {
+
+	public static final String FEATURE_ID = "feature.authorization.oauth2-resource-server";
+
+	private OAuth2ResourceServerFeature(String id, FeatureType featureType, Map<String, Object> options) {
+		super(id, featureType, options);
+	}
+
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	public static final class Builder extends Feature.Builder<OAuth2ResourceServerFeature, Builder> {
+
+		private Builder() {
+			super(FEATURE_ID, FeatureType.AUTHORIZATION);
+		}
+
+		@Override
+		public OAuth2ResourceServerFeature build() {
+			return new OAuth2ResourceServerFeature(getId(), getFeatureType(), getOptions());
+		}
+
+	}
+
+}
